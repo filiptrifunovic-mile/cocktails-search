e888355b20b5df75384ac8662c49a9e294ea5f75
Filip <filip.trifunovic23@gmail.com>
2022-10-18 00:01:37 +0200

    Home,Search, CocktailsList,context Api setup

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,22 @@
-import React from "react";
+import Cocktails from "./Cocktails";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 function CocktailList() {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        No Cocktails matched your search criteria.
+      </h2>
+    );
+  }
+
   return <div>CocktailList</div>;
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,8 @@
-import React from "react";
+import { useGlobalContext } from "../context";
 
 function SearchForm() {
+  const { setSearchTerm } = useGlobalContext();
+
   return <div>SearchForm</div>;
 }
 
